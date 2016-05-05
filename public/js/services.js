23f79150dc119493b2da10d09fcc2f9df362dcc1
David Urbina <durbina1991@gmail.com>
2016-05-04 18:05:14 -0700

    Heroku Revisions

--- public/js/services.js
+++ public/js/services.js
@@ -5,11 +5,11 @@ var app = angular.module("pokedexApp");
 app.service("PokedexServices", function ($http) {
     
     this.getPokedex = function (region) {
-        return $http.get(`http://pokeapi.co/api/v2/pokedex/${region}/`)
+        return $http.get(`https://pokeapi.co/api/v2/pokedex/${region}/`)
     };
 
     this.getPokemonEntry = function (pokemon) {
         console.log(pokemon)
-        return $http.get(`http://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        return $http.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     }
 });
